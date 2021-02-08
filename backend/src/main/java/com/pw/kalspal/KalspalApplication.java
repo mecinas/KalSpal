21ce183d3ee6f4db0d3cf633b43674cf1cb33bc8
damianSmu <smugorzewski.ds@gmail.com>
2021-02-08 22:54:06 +0100

    Backend init

---
+++ backend/src/main/java/com/pw/kalspal/KalspalApplication.java
@@ -0,0 +1,13 @@
+package com.pw.kalspal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalspalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalspalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pw/kalspal/KalspalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pw.kalspal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalspalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
