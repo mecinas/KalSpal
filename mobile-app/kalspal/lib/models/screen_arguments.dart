a3b754bfac25782a43f2f6f9b78aaec00969a5b2
bienkos <sewerynbienko@gmail.com>
2021-02-12 16:24:35 +0100

    Refactor a little ScreenArguments

--- mobile-app/kalspal/lib/models/screen_arguments.dart
+++ mobile-app/kalspal/lib/models/screen_arguments.dart
@@ -1,5 +1,5 @@
 class ScreenArguments {
-  final String workout_type, access_token;
+  final String workoutType, accessToken;
 
-  ScreenArguments(this.workout_type, this.access_token);
+  ScreenArguments(this.workoutType, this.accessToken);
 }
